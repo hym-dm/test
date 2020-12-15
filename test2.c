1795487a2547bd6ab52d3c5a6248b92acaab06d3
Hou Yongming <845376473@qq.com>
2020-12-15 15:34:19 +0800

    测试命令行提交代码

---
+++ test2.c
@@ -0,0 +1,15 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	printf("\a\a\a\a\a");
+//	return 0;
+//}
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	char c = '\45';
+	printf("%c\n",c);
+	return 0;
+}
