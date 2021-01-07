642a07c97684ce43667264efc0b999884c47798c
Hou Yongming <845376473@qq.com>
2021-01-07 21:02:50 +0800

    2021.1.7练习

---
+++ 打印水仙花数(2).c
@@ -0,0 +1,30 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<windows.h>
+//#include<math.h>
+//int main()
+//{
+//	int i = 1;
+//	for (i = 0; i <= 100000; i++)
+//	{
+//		int count = 1;
+//		int tmp = i;
+//		int sum = 0;
+//		while (tmp / 10)
+//		{
+//			count++;
+//			tmp = tmp / 10;
+//		}
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10, count);
+//			tmp = tmp / 10;
+//		}
+//		if (i == sum)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
