b60197a31f5c13951ce19825e31af03d131d77eb
Hou Yongming <845376473@qq.com>
2021-01-27 20:21:11 +0800

    2021-1-25带头双循环链表

---
+++ Sysutil shaungxunhuan.h
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#ifndef _SYSUTIL_H_
+#define _SYSUTIL_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+#include<stdbool.h>
+
+//#include<vld.h>  //内存泄漏工具的头文件
+
+#endif
